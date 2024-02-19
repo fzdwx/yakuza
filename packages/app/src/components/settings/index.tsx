9d34dfbbac3bce2253381f57d0f4a29c76bde5ba
fzdwx <likelovec@gmail.com>
2024-02-19 20:22:55 +0800

    feat: add proxy setting input

--- packages/app/src/components/settings/index.tsx
+++ packages/app/src/components/settings/index.tsx
@@ -1,3 +1,47 @@
-export default ()=>{
-    return <div>Settings</div>
+import {Background, config, Container} from "yakuza-api";
+import {useEffect, useState} from "react";
+import {useViewEvent} from "@/hooks/useView";
+import {useSettingsStore} from "@/hooks/useSettingsStore";
+import {useKeyPress} from "ahooks";
+
+interface Settings {
+    proxy: string
+}
+
+export default () => {
+    const [proxyVal, setProxyVal] = useState("")
+    const {changeView} = useViewEvent()
+    useKeyPress('esc', () => {
+        changeView("self")
+    })
+    useEffect(() => {
+        config.get("yakuza-settings").then((val) => {
+            const s = JSON.parse(val) as Settings
+            setProxyVal(s.proxy)
+        })
+    }, [])
+
+    return <Container>
+        <Background>
+            <div className="p-10">
+                <div className="text-2xl cursor-default">Settings</div>
+                <div className="p-2">
+
+                    <div className="flex">
+                        <h1 className="text-lg mr-2 cursor-default pt-1">HTTP Proxy</h1>
+                        <input type="text" placeholder="eg http://localhost:7890" value={proxyVal} onInput={e=>{
+                            setProxyVal((e.target as HTMLInputElement).value)
+                        }} className="p-2 w-60% rounded-md"/>
+                    </div>
+
+
+                    <button className="bg-#10b981 text-white p-2 mt-2 rounded-md" onClick={()=>{
+                        config.set("yakuza-settings", JSON.stringify({proxy: proxyVal}))
+                        changeView("self")
+                    }}>Save</button>
+                </div>
+
+            </div>
+        </Background>
+    </Container>
 }
