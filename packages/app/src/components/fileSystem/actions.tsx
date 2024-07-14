f8887305f89516b484c180035b9aef0a1787f469
fzdwx <likelovec@gmail.com>
2024-07-14 15:45:40 +0800

    feat: file system add action
    1. open in idea
    2. open in vscode

--- packages/app/src/components/fileSystem/actions.tsx
+++ packages/app/src/components/fileSystem/actions.tsx
@@ -5,12 +5,28 @@ import {Action} from "@/lib/command";
 export function getActions(file: File, fullPath: string, changeVisible: () => void) {
     return [
         {
+            id: `OpenInIdea:${file.name}`,
+            name: 'Open in IntelliJ IDEA',
+            perform: () => {
+                window.launcher.execCommand("idea", [fullPath])
+                changeVisible()
+            }
+        },
+        {
             id: `OpenInFileManager:${file.name}`,
             name: `Open in File Manager`,
             perform: () => {
-                shell.openPath(fullPath)
+                shell.showItemInFolder(fullPath)
                 changeVisible()
             }
-        } as Action
+        } as Action,
+        {
+            id: `OpenInVSCode:${file.name}`,
+            name: 'Open in VSCode',
+            perform: () => {
+                window.launcher.execCommand("code", [fullPath])
+                changeVisible()
+            }
+        },
     ]
 }
